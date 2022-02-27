411922b8ff4a0fea47737b7e3e825bc9a0c4e001
Michal Brzozek <mibrzozek@gmail.com>
2022-02-26 16:21:06 -0800

    maven update 5

--- src/main/java/com/DBSpring/DiscordBotSpring/listeners/PingListenerImp.java
+++ src/main/java/com/DBSpring/DiscordBotSpring/listeners/PingListenerImp.java
@@ -15,7 +15,7 @@ public class PingListenerImp implements PingListener
         }
         else if(messageCreateEvent.getMessageContent().equals(".ding"))
         {
-            messageCreateEvent.getChannel().sendMessage("Dong!");
+            messageCreateEvent.getChannel().sendMessage("Song!");
         }
     }
 }
