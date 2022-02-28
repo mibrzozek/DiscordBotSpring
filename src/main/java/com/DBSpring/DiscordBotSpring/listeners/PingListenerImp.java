6d4ffcbb38fb0a6b37cedcabfa07bca5d9370918
Michal Brzozek <mibrzozek@gmail.com>
2022-02-28 13:37:03 -0800

    ecs service 5

--- src/main/java/com/DBSpring/DiscordBotSpring/listeners/PingListenerImp.java
+++ src/main/java/com/DBSpring/DiscordBotSpring/listeners/PingListenerImp.java
@@ -15,7 +15,7 @@ public class PingListenerImp implements PingListener
         }
         else if(messageCreateEvent.getMessageContent().equals(".ding"))
         {
-            messageCreateEvent.getChannel().sendMessage("This is blong!");
+            messageCreateEvent.getChannel().sendMessage("This is bajong!");
         }
     }
 }
