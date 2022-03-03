904160199bbd1bd52cacea7f9cd3f6f3ba104890
Michal Brzozek <mibrzozek@gmail.com>
2022-03-03 11:54:08 -0800

    database testing 5

--- src/main/java/com/DBSpring/DiscordBotSpring/listeners/PingListenerImp.java
+++ src/main/java/com/DBSpring/DiscordBotSpring/listeners/PingListenerImp.java
@@ -15,7 +15,7 @@ public class PingListenerImp implements PingListener
         }
         else if(messageCreateEvent.getMessageContent().equals(".ding"))
         {
-            messageCreateEvent.getChannel().sendMessage("This is database testing 3!");
+            messageCreateEvent.getChannel().sendMessage("This is database testing 4!");
         }
     }
 }
