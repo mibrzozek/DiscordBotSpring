dc140dd59f0830fa2b78c93b6cdf16db19860366
Michal Brzozek <mibrzozek@gmail.com>
2022-03-07 14:26:00 -0800

    bean configurations 5

--- src/main/java/com/DBSpring/DiscordBotSpring/listeners/PingListenerImp.java
+++ src/main/java/com/DBSpring/DiscordBotSpring/listeners/PingListenerImp.java
@@ -15,7 +15,7 @@ public class PingListenerImp implements PingListener
         }
         else if(messageCreateEvent.getMessageContent().equals(".ding"))
         {
-            messageCreateEvent.getChannel().sendMessage("This is database testing 1000000!");
+            messageCreateEvent.getChannel().sendMessage("This is database testing 2000000!");
         }
     }
 }
