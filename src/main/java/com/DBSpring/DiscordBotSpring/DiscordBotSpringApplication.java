9372afbe1ea076adbabef4471753ee027bdf5d32
Michal Brzozek <mibrzozek@gmail.com>
2022-03-07 12:58:30 -0800

    bean configurations 4

--- src/main/java/com/DBSpring/DiscordBotSpring/DiscordBotSpringApplication.java
+++ src/main/java/com/DBSpring/DiscordBotSpring/DiscordBotSpringApplication.java
@@ -10,7 +10,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
